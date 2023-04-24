aa53e2848c464a89707feb466683e182435dd75a
1988Igor <110403993+1988Igor@users.noreply.github.com>
2023-04-24 14:30:16 +0200

    add changed in mysql config

--- src/main/java/com/example/application/views/about/AboutView.java
+++ src/main/java/com/example/application/views/about/AboutView.java
@@ -24,11 +24,7 @@ import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.data.converter.StringToIntegerConverter;
 import com.vaadin.flow.data.value.ValueChangeMode;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.router.*;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import java.util.Optional;
 
@@ -297,6 +293,9 @@ public class AboutView extends Div implements BeforeEnterObserver {
 
 
     }
+
+
+
 
 
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+
+insert into projects(version, id,project_number,project_name,date_of_beginn,price_netto,price_brutto,status_of_project,comments) values (1, 4,'70','jigrormo','2030-04-01','70','70','jigrormo','no');
